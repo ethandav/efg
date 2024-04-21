634dd43f799a332dc01c44d841dfa89d1945a095
Ethan Davis <121524320+ethandav@users.noreply.github.com>
2024-04-20 17:53:19 -0700

    FPS Counter

--- efg/Engine.cpp
+++ efg/Engine.cpp
@@ -1,5 +1,10 @@
 #include "Engine.h"
 
+#ifdef FPS_COUNTER
+#include <chrono>
+#include <iostream>
+#endif
+
 void Engine::Initialize()
 {
 	window = gfxCreateWindow(1920, 1080, "efg");
@@ -13,10 +18,29 @@ void Engine::Initialize()
 
 void Engine::start()
 {
+#ifdef FPS_COUNTER
+	std::chrono::steady_clock::time_point lastTime = std::chrono::steady_clock::now();
+	int frameCount = 0;
+	double fps = 0.0;
+#endif
+
 	while (!gfxWindowIsCloseRequested(window))
 	{
 		gfxWindowPumpEvents(window);
 		renderer.update();
+#ifdef FPS_COUNTER
+		frameCount++;
+
+		auto currentTime = std::chrono::steady_clock::now();
+		double timeDifference = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastTime).count() / 1000.0;
+		
+		if (timeDifference >= 1.0) {
+		    fps = frameCount / timeDifference;
+		    frameCount = 0;
+		    lastTime = currentTime;
+		    std::cout << "FPS: " << fps << std::endl;
+		}
+#endif
 	}
 }
 
